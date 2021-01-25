662ae34bcafceae32c01fc70407cb7e66e1be8c0
Mamadou Cisse <mciissee@gmail.com>
2021-01-25 23:58:26 +0100

    setup: configure cors issues to allow ng dev server

--- reddit-back/src/main/java/fr/uge/jee/reddit/RedditApplication.java
+++ reddit-back/src/main/java/fr/uge/jee/reddit/RedditApplication.java
@@ -2,10 +2,24 @@ package fr.uge.jee.reddit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class RedditApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RedditApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/**")
+						.allowedOrigins("http://localhost:4200");
+			}
+		};
 	}
 }
